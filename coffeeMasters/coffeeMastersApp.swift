4fc00e9a385c9b8ff05301857e3fb24657d7dc92
Chomba <dev.chomba@gmail.com>
2023-10-22 08:10:41 +0200

    Initial Commit

---
+++ coffeeMasters/coffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  coffeeMastersApp.swift
+//  coffeeMasters
+//
+//  Created by Chomba Chanda on 22/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct coffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
